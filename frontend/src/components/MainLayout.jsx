ccf9b375bd2031d93d0fa252f6c39039afb62b44
Anurag Maurya <132005647+AnuragMaurya42@users.noreply.github.com>
2025-02-22 01:56:15 +0530

    Update MainLayout.jsx left side bar

--- frontend/src/components/MainLayout.jsx
+++ frontend/src/components/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import LeftSidebar from './LeftSidebar';
+import LeftSidebar from './Leftsidebar';
 import RightSidebar from './RightSidebar';
 
 const MainLayout = () => {
